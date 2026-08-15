4bd94547a53bd9a7259601fdf7cc691619f8071e
Bankz51 <osanyintuyia24@gmail.com>
2026-08-15 19:21:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Bankz Comfort Apartment website is working!");
+document.addEventListener("DOMContentLoaded", function () {
+    const bookNow = document.getElementById("bookNow");
+
+    bookNow.addEventListener("click", function () {
+        window.open(
+            "https://wa.me/254759651705?text=Hello%20Bankz%20Comfort%20Apartments,%20I%20would%20like%20to%20book%20a%20stay.",
+            "_blank"
+        );
+    });
+});
