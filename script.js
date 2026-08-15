ce3a81bbd0933d48a6ced1856d715b3fb72f7350
Bankz51 <osanyintuyia24@gmail.com>
2026-08-15 19:39:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-    if (!sessionStorage.getItem("welcomeShown")) {
-        alert("Welcome to Bankz Comfort Apartment! 🏠");
-        sessionStorage.setItem("welcomeShown", "true");
-    }
-});
+alert("Welcome to Bankz Comfort Apartment! 🏠");
