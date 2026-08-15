13607666d7f83bef48f6c4dd4a7b9d132a8ee5ee
Bankz51 <osanyintuyia24@gmail.com>
2026-08-16 02:51:26 +0300

    Add booking confirmation for bookNowTop button

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("click", function (event) {
-    if (event.target.id === "bookNow" || event.target.id === "bookNow2") {
+   if (event.target.id === "bookNow" || event.target.id === "bookNow2" || event.target.id === "bookNowTop") {
         const confirmBooking = confirm(
             "Would you like to contact Bankz Comfort Apartments on WhatsApp to book your stay?"
         );
