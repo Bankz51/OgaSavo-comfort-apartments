61c5d35fc507d2b2e2e65ec78bedb8eeb24243d2
Bankz51 <osanyintuyia24@gmail.com>
2026-08-16 17:00:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,6 @@ document.addEventListener("click", function (event) {
 });
 const bookButton = document.getElementById("bookNow");
 bookButton.textContent = "Reserve Now";
-const apartmentPrice = "KSh 3,500";
+const apartmentPrice = "KSh 3,000";
 document.getElementById("nightlyPrice").textContent = apartmentPrice + " per night";
 document.getElementById("displayPrice").textContent = apartmentPrice;
